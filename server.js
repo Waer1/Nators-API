d4dbe9c5f3fef064e0e69ca1357b3d89ba08e136
Yousef Al-waer <elwaeryousef@gmail.com>
2023-06-04 10:55:53 +0300

    remove th emodel from the main app and separate it into individual module

--- server.js
+++ server.js
@@ -19,34 +19,7 @@ mongoose
   .connect(DB_URL, {
     useNewUrlParser: true,
   })
-  .then(() => console.log('DB connection successful!'));
-
-const tourSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, 'A tour must have a name!'],
-    unique: true,
-  },
-  rating: {
-    type: Number,
-    default: 4.5,
-  },
-  price: {
-    type: Number,
-    required: [true, 'A tour must have a price!'],
-  },
-});
-
-const Tour = mongoose.model('Tour', tourSchema);
-
-const testTour = new Tour({
-  name: 'the Park Camper',
-  price: 998,
-});
-
-testTour
-  .save()
-  .then((doc) => console.log(doc))
+  .then(() => console.log('DB connection successful!'))
   .catch((err) => console.log(err));
 
 const port = process.env.PORT || 3000;
