355d71db7c99dd8c41ceb90ddd0be7a2334cf5f3
Yousef Al-waer <elwaeryousef@gmail.com>
2023-06-04 15:20:38 +0300

    separete the api feature from the main request

--- controllers/tourControllers.js
+++ controllers/tourControllers.js
@@ -1,4 +1,5 @@
 const Tours = require('../models/tourModel');
+const APIFeatures = require('../utils/apiFeatures');
 
 // middleware for
 // exports.checkBody = (req, res, next) => {
@@ -33,52 +34,14 @@ exports.aliasTopTopCheapTours = (req, res, next) => {
 exports.getAllTours = async (req, res) => {
   try {
     // Builing Query
-    // 1) filtering
-    const queryobj = { ...req.query };
-    const excludeFields = ['page', 'sort', 'limit', 'fields'];
-    excludeFields.forEach((ex) => delete queryobj[ex]);
+    const query = new APIFeatures(Tours.find(), req.query)
+      .filter()
+      .sort()
+      .limit()
+      .fields()
+      .paginate();
 
-    // 2) Advanced filtering
-    let queryStr = JSON.stringify(queryobj);
-    const advancedQueryobj = JSON.parse(
-      queryStr.replace(/\b(gt|gte|lt|lte)\b/g, (match) => `$${match}`)
-    );
-
-    let query = Tours.find(advancedQueryobj);
-
-    // 3) Sort results
-    if (req.query.sort) {
-      const sortBy = req.query.sort.split(',').join(' ');
-      query = query.sort(sortBy);
-    } else {
-      query = query.sort('createdAt');
-    }
-
-    let limit = req.query.limit;
-    // 4)LIMIT
-    if (req.query.limit) {
-      query = query.limit(limit);
-    } else {
-      limit = 5;
-    }
-
-    // 5) Fields
-    if (req.query.fields) {
-      const fields = req.query.fields.split(',').join(' ');
-      query = query.select(fields);
-    } else {
-      query = query.select('-__v -_id');
-    }
-
-    // 6)Paggination
-    if (req.query.page) {
-      const numberOfTours = await Tours.countDocuments({});
-      if ((req.query.page - 1) * limit >= numberOfTours)
-        throw new Error('Limit exceeded');
-      query = query.skip((req.query.page - 1) * limit).limit(limit);
-    }
-
-    const tours = await query;
+    const tours = await query.query;
     res.status(200).json({
       status: 'success',
       results: tours.length,
@@ -164,6 +127,93 @@ exports.deleteTour = async (req, res) => {
     res.status(400).json({
       status: 'error',
       message: err,
+    });
+  }
+};
+
+exports.getTourStats = async (req, res) => {
+  try {
+    const stats = await Tours.aggregate([
+      {
+        $match: { ratingsAverage: { $gte: 4.5 } },
+      },
+      {
+        $group: {
+          _id: { $toUpper: '$difficulty' },
+          numTours: { $sum: 1 },
+          numRating: { $sum: '$ratingsQuantity' },
+          avgRating: { $avg: '$ratingsAverage' },
+          avgPrice: { $avg: '$price' },
+          minPrice: { $min: '$price' },
+          maxPrice: { $max: '$price' },
+        },
+      },
+    ]);
+
+    res.status(200).json({
+      status: 'success',
+      data: {
+        tourData: stats,
+      },
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({
+      status: 'error',
+      message: error,
+    });
+  }
+};
+
+exports.getMonthlyStats = async (req, res) => {
+  try {
+    const year = req.params.year;
+    const monthlyStats = await Tours.aggregate([
+      {
+        $unwind: '$startDates',
+      },
+      {
+        $match: {
+          startDates: {
+            $gte: new Date(`${year}-01-01`),
+            $lte: new Date(`${year}-12-31`),
+          },
+        },
+      },
+      {
+        $group: {
+          _id: { $month: '$startDates' },
+          numToursStats: { $sum: 1 },
+          tours: { $push: '$name' },
+        },
+      },
+      {
+        $addFields: { month: '_id' },
+      },
+      {
+        $sort: { numToursStats: -1 },
+      },
+      {
+        $limit: 6,
+      },
+      {
+        $project: {
+          _id: 0,
+        },
+      },
+    ]);
+
+    res.status(200).json({
+      status: 'success',
+      data: {
+        monthlyStats,
+      },
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({
+      status: 'error',
+      message: error,
     });
   }
 };
