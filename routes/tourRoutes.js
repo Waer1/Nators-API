ae79bc173ac565a1366f3bf54b54223d2df43e40
Yousef Al-waer <elwaeryousef@gmail.com>
2023-06-04 10:56:18 +0300

    adding alias for top-5-tours

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -6,16 +6,20 @@ const {
   getTour,
   updateTour,
   deleteTour,
-  checkID,
-  checkBody,
+  aliasTopTopCheapTours,
+  // checkID,
+  // checkBody,
 } = require('../controllers/tourControllers');
 
 const tourRouter = express.Router();
 
-tourRouter.param('id', checkID);
+// usage of the middleware
+// tourRouter.param('id', checkID);
+
+tourRouter.route('/top-5-tour').get(aliasTopTopCheapTours, getAllTours);
 
 tourRouter.route('/:id').get(getTour).patch(updateTour).delete(deleteTour);
 
-tourRouter.route('/').get(getAllTours).post(checkBody, addTour);
+tourRouter.route('/').get(getAllTours).post(addTour);
 
 module.exports = tourRouter;
