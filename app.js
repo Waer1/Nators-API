68ada6bb6716a9a6b7e9e91ed72b4199d05e2bb0
Yousef Al-waer <elwaeryousef@gmail.com>
2023-06-19 16:15:31 +0300

    add security pacages

--- app.js
+++ app.js
@@ -1,25 +1,45 @@
-const fs = require('fs');
-
 const express = require('express');
-
 const morgan = require('morgan');
-
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const hpp = require('hpp');
 const AppError = require('./utils/appError');
-
 const { globalErrorHandler } = require('./controllers/errorController');
-
 const userRouter = require(`./routes/userRoute`);
-
 const tourRouter = require(`./routes/tourRoutes`);
-
 const app = express();
 
 // MIDDLEWARES
+app.use(helmet({}));
+
 if (process.env.NODE_ENV !== 'production') {
   app.use(morgan('dev'));
 }
 
-app.use(express.json());
+const rateLimiter = rateLimit({
+  max: 1000,
+  windowMs: 60 * 60 * 1000,
+  message: 'Rate limit exceeded for one IP',
+});
+app.use('/api', rateLimiter);
+
+app.use(
+  express.json({
+    limit: '10kb',
+  })
+);
+
+app.use(mongoSanitize());
+
+app.use(xss());
+
+app.use(
+  hpp({
+    whitelist: ['duration', 'ratingQuantity', 'price', 'ratingAvarage'],
+  })
+);
 
 app.use(express.static(`${__dirname}/public`));
 
