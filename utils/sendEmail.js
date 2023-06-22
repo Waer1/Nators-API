22d2330de41edd721529ce840e4e803c25086e7f
Yousef Al-waer <elwaeryousef@gmail.com>
2023-06-22 16:52:39 +0300

    sending diffrent en=mail to user and add the pugs

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,26 +1,54 @@
 const nodemailer = require('nodemailer');
+const pug = require('pug');
+const htmlToText = require('html-to-text');
 
-const sendEmail = async (Options) => {
-  // create the transporter
-  const transporter = nodemailer.createTransport({
-    host: process.env.EMAIL_HOST,
-    port: process.env.EMAIL_PORT,
-    auth: {
-      user: process.env.EMAIL_USERNAME,
-      pass: process.env.EMAIL_PASSWORD,
-    },
-  });
+module.exports = class Email {
+  constructor(user, url) {
+    this.to = user.email;
+    this.firstName = user.name.split(' ')[0];
+    this.lastName = user.name.split(' ')[1];
+    this.url = url;
+    this.from = process.env.EMAIL_FROM;
+  }
 
-  // options
-  const mailoptions = {
-    from: process.env.EMAIL_FROM,
-    to: Options.email,
-    subject: Options.subject,
-    text: Options.message,
-  };
+  createTransport() {
+    if (process.env.NODE_ENV === 'production') {
+      // using sendgrid
+      return 1;
+    }
 
-  // send the email
-  await transporter.sendMail(mailoptions);
+    // 1- creating a new transport
+    const transporter = nodemailer.createTransport({
+      host: process.env.EMAIL_HOST,
+      port: process.env.EMAIL_PORT,
+      auth: {
+        user: process.env.EMAIL_USERNAME,
+        pass: process.env.EMAIL_PASSWORD,
+      },
+    });
+  }
+  async send(template, subject) {
+    // render the html html
+    const html = pug.render(`${__dirname}/../views/email/welcome.pug`, {
+      firstName: this.firstName,
+      url: this.url,
+      subject: subject,
+    });
+
+    // define email options
+    const mailoptions = {
+      from: this.from,
+      to: this.to,
+      subject: subject,
+      html: html,
+      text: htmlToText.fromString(html),
+    };
+
+    // create transport and send email
+    await this.createTransport().sendMail(mailoptions);
+  }
+
+  async sendWelcome() {
+    await this.send('Welcome!!', 'Welcome to the nators family!');
+  }
 };
-
-module.exports = sendEmail;
